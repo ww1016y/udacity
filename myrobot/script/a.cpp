6a541ac2b987d334051150747cc4bbbf8e485084
ww1016y <ww1016y@gmail.com>
2019-07-25 21:20:34 +0900

    byeok

--- myrobot/script/a.cpp
+++ myrobot/script/a.cpp
@@ -3,101 +3,81 @@
 #include <queue>
 #include <string.h>
 #include <vector>
-#define MAX 11
+#define W_MAX 12
+#define H_MAX 15
 using namespace std;
-int map[MAX][MAX][8];
-int M,A;
-int user[2][101];
-vector<pair<int, int>> user1;
-vector<pair<int, int>> user2;
-vector<pair<int, int>> BC;
-vector<pair<int, int>> BC_info;
-int power[8];
+int map[H_MAX][W_MAX];
+int ex[H_MAX][W_MAX];
 
-void cover(int a, int b, int i, int c, int d){
-	
-	int visit[MAX][MAX];
-	memset(visit,0,sizeof(visit));
-	queue<pair<int, int>> q;
-	q.push(make_pair(a,b));
-	visit[a][b] = 0;
 
-	while(!q.empty()){
-		int x =q.front().first;		
-		int y =q.front().second;
-		q.pop();
-		for(int j=0; j<4; j++){
-			int nx = x +dx[j];
-			int ny = y +dy[j];
-			if (nx < 1 || ny < 1 || nx > 10 || ny > 10) {
-				continue;
+int N, W, H;
+
+void boom(int j, int i){
+
+	int x = j;
+	int y = i;
+
+	int until=ex[j][i];
+	ex[j][i] = 0;
+	for (int k = 0; k<4; k++){
+		for (int l =0; l<until-1; l++){
+			x = x + dx[k];
+			y = y + dy[k];
+			if (ex[x][y] > 0){
+				boom(x,y);
 			}
-			if (visit[x][y] + 1 > c){
-				continue;
-			}
-			visit[nx][ny] = visit[x][y] + 1;
-			map[nx][ny][i] = d;
+
 		}
 	}
-
 }
 
-void trajectory(){
+void rearrange(){
 
-	int u1x =1; int u1y =1; int u2x=10; int u2y = 10;
-	user1.push_back(make_pair(u1x,u1y));
-	user2.push_back(make_pair(u1y,u2y));
+	for (int i = 0; i < W; i++) {
+	    	for (int j = 0; j < H; j++) {
+			if ( ex[j][i] > 0) {
+				for (int k = j+1; k < H; k++) {
+					if ( ex[k][i] == 0) {
+						ex[k][i]  = ex[j][i];			
+					}	   
+				}   				
+			}			
+		}
+	}
+}
 
-	for (int i = 0; i <= 1; i++) {
-	    	for (int j = 0; j <= M; j++) {
-			if (user[i][j] == 0) {
-				if (i == 0) {
-					user1.push_back(make_pair(u1x,u1y));
+void solve(int count, int temp[]){
+
+	if (count == 4) {
+
+		return;
+	}
+
+	for (int i = 0; i < W; i++) {
+	    	for (int j = 0; j < H; j++) {
+			if ( temp[j][i] > 0) {
+				
+				for (int a = 0; a < H; a++) {
+				    	for (int b = 0; b < W; b++) {
+						ex[i][j] = temp[a][b];	    
+					}
 				}
-				else if (i == 1) {
-					user2.push_back(make_pair(u2x,u2y));
+				boom(j,i);
+
+				rearrange();
+			
+
+				for (int a = 0; a < H; a++) {
+				    	for (int b = 0; b < W; b++) {
+						temp[i][j] = ex[a][b];	    
+					}
 				}
-			}
-			else if (user[i][j] == 1) { // 상우하좌
-				if (i == 0) {
-					u1x = u1x - 1;
-					user1.push_back(make_pair(u1x,u1y));
-				}
-				else if (i == 1) {
-					u2x = u2x - 1;
-					user2.push_back(make_pair(u2x,u2y));
-				}
-			}
-			else if (user[i][j] == 2) { 
-				if (i == 0) {
-					u1y = u1y + 1;
-					user1.push_back(make_pair(u1x,u1y));
-				}
-				else if (i == 1) {
-					u2y = u2y + 1;
-					user2.push_back(make_pair(u2x,u2y));
-				}
-			}
-			else if (user[i][j] == 3) { 
-				if (i == 0) {
-					u1x = u1x + 1;
-					user1.push_back(make_pair(u1x,u1y));
-				}
-				else if (i == 1) {
-					u2x = u2x + 1;
-					user2.push_back(make_pair(u2x,u2y));
-				}
-			}
-			else if (user[i][j] == 4) { 
-				if (i == 0) {
-					u1y = u1y - 1;
-					user1.push_back(make_pair(u1x,u1y));
-				}
-				else if (i == 1) {
-					u2y = u2y - 1;
-					user2.push_back(make_pair(u2x,u2y));
-				}
-			}
+
+
+
+				solve(count+1,temp);
+	
+			}	   
 		}
 	}
 
@@ -108,51 +88,19 @@ int main() {
     scanf("%d", &t);
     for (int tc = 1; tc <= t; tc++) {
         
-        scanf("%d %d ", &M, &A);
-	memset(user,0,sizeof(user));
-	user1.clear(); user2.clear(); BC.clear(); BC_info.clear();
+        scanf("%d %d %d", &N, &W, &H);
+	
+	memset(map, 0, sizeof(map));
+	int temp[H_MAX][W_MAX];
 
-	for (int i = 0; i <= 1; i++) {
-	    	for (int j = 0; j <= M; j++) {
-			scanf("%d", &user[i][j]);	    
+	for (int i = 0; i < H; i++) {
+	    	for (int j = 0; j < W; j++) {
+			scanf("%d", &map[i][j]);
+			temp[i][j] = map[i][j];	    
 		}
 	}
 
-        trajectory();
-	
-	for (int i = 0; i < A; i++) {
-	    int a,b,c,d;
-	    scanf("%d %d %d %d ", &a, &b, &c, &d);
-	    BC.push_back(make_pair(a,b));	    
-	    BC_info.push_back(make_pair(c,d));
-        }
-	
-	for (int i = 0; i < A; i++) {
-		cover(BC[i].first,BC[i].second,i,BC_info[i].first,BC_info[i].second);	  
-        }	
-	
-	int res1[8]; int index1 = 0; int sum =0;
-	memset(res1,0,sizeof(res1));
-
-	for (int i = 0; i <= M; i++) {
-		for (int j =0; j<A; j++){
-			if (map[user1[i].first][user1[i].second][j] != 0) {
-				index1 = j;
-				for (int j =0; j<A; j++){
-					if (map[user2[i].first][user2[i].second][j] != 0) {
-						if ( index1 == j) {
-							temp = map[user1[i].first][user1[i].second][j];
-						}
-						else temp = map[user1[i].first][user1[i].second][j] + map[user2[i].first][user2[i].second][j];
-					}
-					if (res1[i] < temp) res1[i] =temp;
-				}
-			}
-		}
-	}
-	for (int i =0; i<=M; i++) {
-		sum = sum+res1[i];
-	}
+        solve(1,temp);
 
         printf("#%d %d\n", tc, sum);
     }
