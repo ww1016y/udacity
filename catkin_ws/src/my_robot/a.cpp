889bca988f7169b36983c16f3b16d15cbf80c16d
ww1016y <39937675+ww1016y@users.noreply.github.com>
2019-09-03 02:30:43 +0900

    Update a.cpp

--- catkin_ws/src/my_robot/a.cpp
+++ catkin_ws/src/my_robot/a.cpp
@@ -1,282 +1,77 @@
 #include <iostream>
 #include <stdio.h>
+#include <string.h>
 #include <vector>
 #include <queue>
 
 using namespace std;
 
-#define MAX 21
-int N;
+#define MAX 11
+int N, C, M;
 int ans;
 
 int map[MAX][MAX];
 int visit[MAX][MAX];
 
-void copy(int a[][MAX], int b[][MAX]) {
+int dx[] = {-1,1,0,0};
+int dy[] = {0,0,1,-1};
+vector <pair<int, int>> v;
+
+void check(){
+	
 	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			a[i][j] = b[i][j];
+			for (int j = 0; j < N; j++) {
+				if (visit[i][j] == 1){
+					if (map[i][j] <= C ){
+						map[i][j]
+						sum = sum + map[i][j];
+						profit = profit + map[i][j]*map[i][j];
+					}
+				}
+			}
 		}
-	}
+
 }
 
-void solve(int index) {
-
-	if (index == 1) {
+void solve(int x, int y) {
 	
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (map[i][j] > ans) {
-					ans = map[i][j];
-				}
-				cout << map[i][j];
+	for (int i = x; i < N; i++) {
+		for (int j = y; j < N-M+1; j++) {
+			if (j+M-1<N){
+				visit[i][j] = 2;
+				check();
+				visit[i][j] = 0;
 			}
-			cout << '\n';
 		}
-
-		return;
 	}
 
-	int temp[MAX][MAX] = {0};
-	copy(temp, map);
-	cout << index << '\n';
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			
-			//cout << temp[i][j];
-		}
-		//cout << '\n';
-	}
-	//cout << '\n';
-	for (int k = 0; k < 4; k++) {
-		
-		if (k == 0) {
-			for (int i = 0; i < N; i++) {
-				queue <int> q;
-				for (int j = N - 1 ; j >= 0; j--) {
-					if (map[i][j] > 0) {
-						q.push(map[i][j]);
-					}
-				}
-				int temp = q.size();
-				int pre = 0;
-				
-				for (int j = 0; j < temp; j ++) {
-					if (pre == 0) {
-						pre = q.front();
-						q.pop();
-						if (j == temp - 1) {
-							q.push(pre);
-						}
-					}
-					else {
-						int num = q.front();
-						q.pop();
-						if (pre == num) {
-							q.push(num * 2);
-							pre = 0;
-						}
-						else {
-							
-							if (j == temp - 1) {
-								q.push(pre);
-								q.push(num);
-							}
-							else {
-								q.push(pre);
-							}
-							pre = num;
-						}
-					}
-				}
-				for (int j = N - 1; j >= 0; j--) {
-					if (!q.empty()) {
-						map[i][j] = q.front();
-						q.pop();
-					}
-					else {
-						map[i][j] = 0;
-					}
-				}
-			}
-			
-			solve(index + 1);
-		}
-		else if (k == 1) {
-			for (int i = 0; i < N; i++) {
-				queue <int> q;
-				for (int j = N - 1; j >= 0; j--) {
-					if (map[j][i] > 0) {
-						q.push(map[j][i]);
-						
-					}
-				}
-				int temp = q.size();
-				int pre = 0;
-
-				for (int j = 0; j < temp; j++) {
-					if (pre == 0) {
-						pre = q.front();
-						q.pop();
-						if (j == temp - 1) {
-							q.push(pre);
-						}
-					}
-					else {
-						int num = q.front();
-						q.pop();
-						if (pre == num) {
-							q.push(num * 2);
-							pre = 0;
-						}
-						else {
-
-							if (j == temp - 1) {
-								q.push(pre);
-								q.push(num);
-							}
-							else {
-								q.push(pre);
-							}
-							pre = num;
-						}
-					}
-				}
-				for (int j = N - 1; j >= 0; j--) {
-					if (!q.empty()) {
-						map[j][i] = q.front();
-						
-						q.pop();
-					}
-					else {
-						map[j][i] = 0;
-					}
-				}
-			}
-			solve(index + 1);
-		}
-		else if (k == 2) {
-			for (int i = 0; i < N; i++) {
-				queue <int> q;
-				for (int j = 0; j <N; j++) {
-					if (map[i][j] > 0) {
-						q.push(map[i][j]);
-					}
-				}
-				int temp = q.size();
-				int pre = 0;
-
-				for (int j = 0; j < temp; j++) {
-					if (pre == 0) {
-						pre = q.front();
-						q.pop();
-						if (j == temp - 1) {
-							q.push(pre);
-						}
-					}
-					else {
-						int num = q.front();
-						q.pop();
-						if (pre == num) {
-							q.push(num * 2);
-							pre = 0;
-						}
-						else {
-
-							if (j == temp - 1) {
-								q.push(pre);
-								q.push(num);
-							}
-							else {
-								q.push(pre);
-							}
-							pre = num;
-						}
-					}
-				}
-				for (int j = 0; j < N; j++) {
-					if (!q.empty()) {
-						map[i][j] = q.front();
-						q.pop();
-					}
-					else {
-						map[i][j] = 0;
-					}
-				}
-			}
-
-			solve(index + 1);
-		}
-		else if (k == 3) {
-			for (int i = 0; i < N; i++) {
-				queue <int> q;
-				for (int j = 0; j < N; j++) {
-					if (map[j][i] > 0) {
-						q.push(map[j][i]);
-
-					}
-				}
-				int temp = q.size();
-				int pre = 0;
-
-				for (int j = 0; j < temp; j++) {
-					if (pre == 0) {
-						pre = q.front();
-						q.pop();
-						if (j == temp - 1) {
-							q.push(pre);
-						}
-					}
-					else {
-						int num = q.front();
-						q.pop();
-						if (pre == num) {
-							q.push(num * 2);
-							pre = 0;
-						}
-						else {
-
-							if (j == temp - 1) {
-								q.push(pre);
-								q.push(num);
-							}
-							else {
-								q.push(pre);
-							}
-							pre = num;
-						}
-					}
-				}
-				for (int j = 0; j < N; j++) {
-					if (!q.empty()) {
-						map[j][i] = q.front();
-
-						q.pop();
-					}
-					else {
-						map[j][i] = 0;
-					}
-				}
-			}
-			solve(index + 1);
-		}
-		copy(map, temp);
-	}
-		
 	return;
 }
 
 int main() {
+	int T;
+	cin >> T;
+	for(int t = 0; t <T; t++){
+		cin >> N >> M >> C;
+		ans = 0;
 
-	cin >> N;
-	ans = 0;
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			cin >> map[i][j];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				cin >> map[i][j];
+			}
 		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N-M+1; j++) {
+				if (j+M-1<N){
+					visit[i][j] = 1;
+					solve(i,j+M);
+					visit[i][j] = 0;
+				}
+			}
+		}
+		
+
+		cout << '#' << t+1 << ' ' << ans;
 	}
-	
-	solve(0);
-	cout << ans;
 }
