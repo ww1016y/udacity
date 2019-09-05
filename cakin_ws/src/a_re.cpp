1562801800556f924b86013a0efe6612ccd8633b
ww1016y <39937675+ww1016y@users.noreply.github.com>
2019-09-05 18:04:52 +0900

    Update a_re.cpp

--- cakin_ws/src/a_re.cpp
+++ cakin_ws/src/a_re.cpp
@@ -1,199 +1,97 @@
-//미네랄 2933
-#include <stdio.h>
 #include <iostream>
-#include <queue>
-#include <string.h>
+#include <stdio.h>
 #include <vector>
-#include <algorithm>
-#define MAX 101
+#include <queue>
+
 using namespace std;
-int map[MAX][MAX];
-int R, C, N;
-int mineral[MAX];
-int dx[4] = {0,1,0,-1}; // 1,2,3,4 상 하 좌 우
-int dy[4] = {1,0,-1,0};
+
+#define MAX 17
+int N;
 int ans;
-int num[6];
-int v[6][4];
+int map[MAX][MAX];
+int visit[MAX][MAX];
+int check[MAX][MAX][MAX][MAX];
+int dx[] = { 0,1,0,-1 };
+int dy[] = { 1,0,-1,0 };
 
-void solve(){
 
-	for (int index = 1; index<=N ; index++) {
-		if (index % 2 == 1) {
-			for (int j = 1; j <= C; j++) {
-				if (map[R-mineral[index]+1][j] == 1){
-					map[R-mineral[index]+1][j] = 0;
-					break;
-				}
-				else continue;		
-			}	
-		}
-		else if (index % 2 == 0) {
-			for (int j = C; j >= 1; j--) {
-				if (map[R-mineral[index]+1][j] == 1){
-					map[R-mineral[index]+1][j] = 0;
-					break;
-				}
-				else continue;		
-			}	
-		}
+void solve(int x, int y, int state, int index) {
 
-		///////////////////////////////////////////
-		int visit[MAX][MAX] = {0};
-		int count = 0;
-		for (int i = 1; i <= R; i++) {
-			for (int j = 1; j <= C; j++) {
-				if (map[i][j] == 1 && visit[i][j] == 0) {
-					count = count + 1;
-					queue <pair<int, int> > q;
-					q.push(make_pair(i,j));
-					visit[i][j] = count;
-					while(!q.empty()){
-						int x = q.front().first;
-						int y = q.front().second;
-						q.pop();
-						for (int k = 0; k<4; k++){
-							int nx = x + dx[k];
-							int ny = y + dy[k];
-							if (nx < 1 || ny < 1 || nx > R || ny > C || map[nx][ny] == 0 || visit[nx][ny] != 0){
-								continue;
-							}
-							if (map[nx][ny] == 1 ){
-								q.push(make_pair(nx,ny));
-								visit[nx][ny] = count;
-							}
-						}
-					}			
-				}
-			}
-		}
-		
-		
-		
-		if (count == 1){
-			;
-		}
-		else {
-			
-				
-				
-			int check[100000]={0}; 
-			for (int j = 1; j <= C; j++) {
-				if (visit[R][j] != 0){
-					check[visit[R][j]] = 1;
-				}		
-			}
-			int point = 0;
-			for (int c = 1; c <= count ; c++){
-				if (check[c] == 0){
-					point = c;
-				}
-			}
-		
-			if (point !=0 ) {
-				//cout << point<<'\n';
-				int m[MAX]= {0};
-					for (int j = 1; j <= C; j++) {
-						for (int i = R; i >= 1; i--) {
-							if (visit[i][j] == point){
-								 m[j]=i;
-								 break;
-							}
-						}
-						//cout << 'j'<< j<< ' '<< m[j] << '\n';
-							
-					}
-					
-					int change = 0;
-					int flag = 0;
-					int move_val = 101;
-					for (int j = 1; j <= C; j++) {	
-						if (m[j] > 0)	{
-							int k = 1;
-							while (1) {
-								if (m[j]+k < R ){
-									if (map[m[j]+k][j] == 0) {
-										k=k+1;
-									}
-									else {
-										k = k- 1;
-										break;
-									}
-								}
-								else if(m[j]+k == R){
-									if (map[m[j]+k][j] == 0) {
-										break;
-									}
-									else {
-										k = k- 1;
-										break;
-									}
-								}
-//								cout << m[j]+k<<'\n';
-							}			
-							//cout << k <<'\n';
-							int temp = k;
-							if ( temp < move_val ) move_val =temp;
-						}
-						else continue;
-						//cout << k <<'\n';
-						
-					}
-				//	cout << move_val << '\n';
-					int t[MAX][MAX]={0};
-			
-					for (int i = R; i >= 1; i--) {
-						for (int j = 1; j <= C; j++) {
-							if (visit[i][j] == point){
-								t[i+move_val][j] = map[i][j];
-							}
-							else {
-								t[i][j] = map[i][j];
-							}
-						}
-					}
-					
-					for (int i = R; i >= 1; i--) {
-						for (int j = 1; j <= C; j++) {
-							map[i][j] = t[i][j];
-						}
-					}
-			}			
-		}	
+	if (x == N && y == N) {
+		ans = ans + 1;
+		return;
 	}
-		
-	for (int i = 1; i <= R; i++) {
-		for (int j = 1; j <= C; j++) {
-			if (map[i][j] == 0) cout << '.';
-			else if (map[i][j] == 1) cout << 'x';
+	
+	if (state == 2) {
+		int nx = x;
+		int ny = y;
+
+		if (ny + 1 <= N) {
+			if (map[nx][ny + 1] == 0) {
+				solve(nx, ny + 1, state, index + 1);
+			}
 		}
-		cout << '\n';
+
+		if (nx + 1 <= N && ny + 1 <= N) {
+			if (map[nx][ny + 1] == 0 && map[nx+1][ny] == 0 && map[nx+1][ny + 1] == 0) {
+				solve(nx+1, ny + 1, 4, index + 1);
+			}
+		}
 	}
+	
+	else if (state == 3) {
+		int nx = x;
+		int ny = y;
+
+		if (nx + 1 <= N) {
+			if (map[nx+1][ny] == 0) {
+				solve(nx+1, ny, state, index + 1);
+			}
+		}
+
+		if (nx + 1 <= N && ny + 1 <= N) {
+			if (map[nx][ny + 1] == 0 && map[nx + 1][ny] == 0 && map[nx + 1][ny + 1] == 0) {
+				solve(nx + 1, ny + 1, 4, index + 1);
+			}
+		}
+	}
+
+	else if (state == 4) {
+		int nx = x;
+		int ny = y;
+
+		if (ny + 1 <= N) {
+			if (map[nx][ny + 1] == 0) {
+				solve(nx, ny + 1, 2, index + 1);
+			}
+		}
+		if (nx + 1 <= N) {
+			if (map[nx + 1][ny] == 0) {
+				solve(nx + 1, ny, 3, index + 1);
+			}
+		}
+		if (nx + 1 <= N && ny + 1 <= N) {
+			if (map[nx][ny + 1] == 0 && map[nx + 1][ny] == 0 && map[nx + 1][ny + 1] == 0) {
+				solve(nx + 1, ny + 1, 4, index + 1);
+			}
+		}
+	}
+
+	return;
 }
 
 int main() {
-    int t=1;
-    //scanf("%d", &t);
-    for (int tc = 1; tc <= t; tc++) {
-	
-	ans = 0; 
-    scanf("%d %d", &R, &C);
-	
-	for (int i = 1; i <= R; i++) {
-		for (int j = 1; j <= C; j++) {
-			char a;
-			cin >> a;
-			if (a == '.') map[i][j] = 0;
-			if (a == 'x') map[i][j] = 1;
+
+	cin >> N ;
+	ans = 0;
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			cin >> map[i][j];
 		}
 	}
-	scanf("%d", &N);
-	for (int i = 1; i <= N; i++) {
-		cin >> mineral[i] ; 	
-	}
 	
-	solve();
-        	
-   // printf("%d", ans);
-    }
+	solve(1,2,2,0);
+
+	cout << ans;
 }
